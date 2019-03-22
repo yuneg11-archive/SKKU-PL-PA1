97725e076d661acb3a884136813484e0ae1c5674
Eunggu Yun <yuneg11@gmail.com>
2019-03-22 15:09:08 +0900

    Add assign logic and disable logs

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -31,61 +31,66 @@ class CalculatorWalker extends CalculatorBaseListener {
 
     @Override
     public void enterProgram(CalculatorParser.ProgramContext ctx) {
-        System.out.println("Entering Program");
+        //System.out.println("Entering Program");
     }
 
     @Override
     public void exitProgram(CalculatorParser.ProgramContext ctx) {
-        System.out.println("Exiting Program");
+        //System.out.println("Exiting Program");
     }
 
     @Override
     public void enterExpression(CalculatorParser.ExpressionContext ctx) {
-        System.out.println("Entering Expression");
+        //System.out.println("Entering Expression");
     }
 
     @Override
     public void exitExpression(CalculatorParser.ExpressionContext ctx) {
-        System.out.println("Exiting Expression");
+        //System.out.println("Exiting Expression");
     }
-    
+
     @Override
     public void enterAssign(CalculatorParser.AssignContext ctx) {
-        System.out.println("Entering Assign");
+        //System.out.println("Entering Assign");
     }
     
     @Override
     public void exitAssign(CalculatorParser.AssignContext ctx) {
-        System.out.println("Exiting Assign");
+        if(ctx.ID() != null && ctx.number() != null) {
+            try {
+                double value = Double.parseDouble(ctx.number().getChild(0).getText());
+                variables.put(ctx.ID().getText(), value);
+            } catch(Exception e) { }
+        }
     }
-    
+
     @Override
     public void enterNumber(CalculatorParser.NumberContext ctx) {
-        System.out.println("Entering Number");
+        //System.out.println("Entering Number");
     }
-    
+
     @Override
     public void exitNumber(CalculatorParser.NumberContext ctx) {
-        System.out.println("Exiting Number");
+        //System.out.println("Exiting Number");
     }
-    
+
     @Override
     public void enterEveryRule(ParserRuleContext ctx) {
-        System.out.println("Entering Every Rule");
+        //System.out.println("Entering Every Rule");
     }
-    
+
     @Override
     public void exitEveryRule(ParserRuleContext ctx) {
-        System.out.println("Exiting Every Rule");
+        //System.out.println("Exiting Every Rule");
     }
-    
+
     @Override
     public void visitTerminal(TerminalNode node) {
-        System.out.println("Visiting Terminal");
+        //System.out.println("Visiting Terminal");
     }
     
     @Override
     public void visitErrorNode(ErrorNode node) {
-        System.out.println("Visiting Error Node");
+        //System.out.println("Visiting Error Node");
     }
 }
