60f487c95e607030de4c65c73a6396a61e634dce
Eunggu Yun <yuneg11@gmail.com>
2019-03-27 13:09:38 +0900

    Add assign logic and disable logs

--- Shunting-yard-based/ExprEvalApp.java
+++ Shunting-yard-based/ExprEvalApp.java
@@ -1,5 +1,6 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ExprEvalApp {
@@ -24,64 +25,70 @@ public class ExprEvalApp {
 }
 
 class ExprWalker extends ExprBaseListener {
+    HashMap<String, Double> variables = new HashMap<String, Double>();
 
     @Override
     public void enterProgram(ExprParser.ProgramContext ctx) {
-        System.out.println("Entering Program");
+        //System.out.println("Entering Program");
     }
 
     @Override
     public void exitProgram(ExprParser.ProgramContext ctx) {
-        System.out.println("Exiting Program");
+        //System.out.println("Exiting Program");
     }
 
     @Override
     public void enterExpression(ExprParser.ExpressionContext ctx) {
-        System.out.println("Entering Expression");
+        //System.out.println("Entering Expression");
     }
 
     @Override
     public void exitExpression(ExprParser.ExpressionContext ctx) {
-        System.out.println("Exiting Expression");
+        //System.out.println("Exiting Expression");
     }
 
     @Override
     public void enterAssign(ExprParser.AssignContext ctx) {
-        System.out.println("Entering Assign");
+        //System.out.println("Entering Assign");
     }
 
     @Override
     public void exitAssign(ExprParser.AssignContext ctx) {
-        System.out.println("Exiting Assign");
+        if(ctx.ID() != null && ctx.number() != null) {
+            try {
+                double value = Double.parseDouble(ctx.number().getChild(0).getText());
+                variables.put(ctx.ID().getText(), value);
+            } catch(Exception e) { }
+        }
     }
 
     @Override
     public void enterNumber(ExprParser.NumberContext ctx) {
-        System.out.println("Entering Number");
+        //System.out.println("Entering Number");
     }
 
     @Override
     public void exitNumber(ExprParser.NumberContext ctx) {
-        System.out.println("Exiting Number");
+        //System.out.println("Exiting Number");
     }
 
     @Override
     public void enterEveryRule(ParserRuleContext ctx) {
-        System.out.println("Entering Every Rule");
+        //System.out.println("Entering Every Rule");
     }
 
     @Override
     public void exitEveryRule(ParserRuleContext ctx) {
-        System.out.println("Exiting Every Rule");
+        //System.out.println("Exiting Every Rule");
     }
 
     @Override
     public void visitTerminal(TerminalNode node) {
-        System.out.println("Visiting Terminal");
+        //System.out.println("Visiting Terminal");
     }
 
     @Override
     public void visitErrorNode(ErrorNode node) {
-        System.out.println("Visiting Error Node");
+        //System.out.println("Visiting Error Node");
     }
 }
